1f97481160a06db5f29eb8dd6fb60c9396a30097
Anupam Karmakar <anupamkarmakar27@gmail.com>
2025-10-23 16:34:05 +0530

    add new

---
+++ 0151-reverse-words-in-a-string/withoutExtraSpace.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        reverse(s.begin(),s.end());
+        int left=0,right=0,i=0;
+        int n=s.size();
+
+        while(i<n){
+            while(i<n && s[i]==' '){
+                i++;
+            }
+            if(i==n) break;
+
+            while(i<n && s[i]!=' '){
+                s[right++]=s[i++];
+            }
+
+            reverse(s.begin()+left ,s.begin()+right);
+            s[right++]= ' ';
+            i++;
+
+            left=right;
+        }
+        s.resize(right-1);
+        return s;
+
+    }
+};
