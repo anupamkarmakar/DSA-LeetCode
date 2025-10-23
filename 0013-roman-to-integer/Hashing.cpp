545ca2c4f4b7c7f7ed12277d493ad32cd3fcbeb5
Anupam Karmakar <anupamkarmakar27@gmail.com>
2025-10-23 23:32:54 +0530

    add

---
+++ 0013-roman-to-integer/Hashing.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int n=s.size();
+        unordered_map <char,int> roman = {{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
+
+        int result=0;
+        for(int i=0;i<n;i++){
+            int curr=roman[s[i]];
+            int next=(i<n-1) ? roman[s[i+1]] : 0;
+
+            if(curr<next) result-=curr;
+            else result+=curr;
+        }
+        return result;
+    }
+};
