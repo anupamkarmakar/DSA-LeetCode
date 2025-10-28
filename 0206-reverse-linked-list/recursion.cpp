a10de0e3b46ed67ef01e532e0450421c10c2b22c
Anupam Karmakar <anupamkarmakar27@gmail.com>
2025-10-28 17:07:04 +0530

    add

---
+++ 0206-reverse-linked-list/recursion.cpp
@@ -0,0 +1,19 @@
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        // Base Case
+        if(head==NULL || head->next==NULL) return head;
+
+        // Recursive function go till null and store last node as a 'newHead'
+        ListNode* newNode = reverseList(head->next);
+
+        // Create 'front' before 'head' node, then reverse links
+        ListNode* front=head->next;
+        front->next=head;
+        head->next=NULL;
+        
+        // it step by step return newNode, after all the returing calls are happened,it return answer
+        return newNode;
+    }
+};
