357511c94236d85dce6b069fa1f7222d2a2a8e80
Anupam Karmakar <anupamkarmakar27@gmail.com>
2025-11-05 20:05:53 +0530

    Time: 0 ms (100%), Space: 27.4 MB (8.16%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n=height.size();
+        vector<int> rmax(n,0);
+        vector<int> lmax(n,0);
+        rmax[0]=height[0], lmax[n-1]=height[n-1];
+
+        for(int i=1;i<n;i++){
+            rmax[i]= max(rmax[i-1],height[i]);
+        }
+        for(int i=n-2;i>=0;i--){
+            lmax[i]= max(lmax[i+1],height[i]);
+        }
+
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans+=min(lmax[i],rmax[i])-height[i];
+        }
+        return ans;
+    }
+};
