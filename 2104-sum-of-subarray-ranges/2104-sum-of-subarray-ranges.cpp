1ec14ac2c7b0abb06569423c6f3d1a5a68709e3a
Anupam Karmakar <anupamkarmakar27@gmail.com>
2025-11-08 09:11:07 +0530

    Time: 0 ms (100%), Space: 15.5 MB (64.66%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -2,17 +2,49 @@ class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
         int n=nums.size();
-        long long ans=0;
+        long long res=0;
+        vector<int> minPrev(n,-1);
+        vector<int> maxPrev(n,-1);
+        vector<int> minNext(n,-1);
+        vector<int> maxNext(n,-1);
+        stack<int> st;
+
+        // Create previous minimum ele of array
+        for(int i=0;i<n;i++){
+            while(!st.empty() && nums[st.top()]>=nums[i]) st.pop();
+            minPrev[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+        while(!st.empty()) st.pop();
+
+        // Next minimum ele of array
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && nums[st.top()]>nums[i]) st.pop();
+            minNext[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+        while(!st.empty()) st.pop();
+
+        // Create previous maximum ele of array
+        for(int i=0;i<n;i++){
+            while(!st.empty() && nums[st.top()]<=nums[i]) st.pop();
+            maxPrev[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+        while(!st.empty()) st.pop();
+
+        // Next maximum ele of array
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && nums[st.top()]<nums[i]) st.pop();
+            maxNext[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
 
         for(int i=0;i<n;i++){
-            int small=nums[i],large=nums[i];
-            for(int j=i;j<n;j++){
-                small = min(small,nums[j]);
-                large = max(large,nums[j]);
-                int diff=(large-small);
-                ans+=diff;
-            }
+            long long leftMin=i-minPrev[i],rightMin=minNext[i]-i;
+            long long leftMax=i-maxPrev[i],rightMax=maxNext[i]-i;
+            res+=(leftMax*rightMax-leftMin*rightMin)*nums[i];
         }
-        return ans;
+        return res;
     }
 };
