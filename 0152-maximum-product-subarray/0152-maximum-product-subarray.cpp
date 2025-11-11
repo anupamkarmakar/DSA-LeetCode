dd2aab40918d106999a53363df4720a71fb3e8d7
Anupam Karmakar <anupamkarmakar27@gmail.com>
2025-11-11 12:08:03 +0530

    Time: 0 ms (100%), Space: 17.8 MB (59.38%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
+    // kandane's Algorithm
     int maxProduct(vector<int>& nums) {
         int n=nums.size();
-        int ans=0;
-        if(n==1) return nums[0];
-        for(int i=0;i<n;i++){
-            int pdt=1;
-            for(int j=i;j<n;j++){
-                pdt*=nums[j];
-                ans=max(ans,pdt);
-            }
+        int maxPdt=nums[0],minPdt=nums[0],ans=nums[0];
+
+        for(int i=1;i<n;i++){
+            if(nums[i]<0) swap(maxPdt,minPdt); // handle negative number
+
+            maxPdt=max(nums[i], nums[i]*maxPdt); // compare with nums[i],bcz start new subarray not extent subarray
+            minPdt=min(nums[i], nums[i]*minPdt);
+
+            ans=max(ans,maxPdt);
         }
         return ans;
     }
