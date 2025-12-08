34373e36e9d840a3600d5480bbb74e85345e840a
Anupam Karmakar <anupamkarmakar27@gmail.com>
2025-12-09 00:32:36 +0530

    add

---
+++ 0700-search-in-a-binary-search-tree/Iterative.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        while(root!=nullptr){
+            if(root->val==val) return root;
+            else if(root->val<val) root=root->right;
+            else root=root->left;
+        }
+        return nullptr;
+    }
+};
