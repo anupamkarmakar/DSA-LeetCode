dfb8a2cf854fe52b6e3f5b39bab6c4005fa58211
Anupam Karmakar <anupamkarmakar27@gmail.com>
2026-03-07 00:52:56 +0530

    Time: 0 ms (100%), Space: 11.5 MB (42.76%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+
+        vector <int> hash(256,0);
+        for(char ch : t) hash[ch]++;
+
+        int minlen = INT_MAX, startIdx = -1, count = 0;
+        int l=0, r=0;
+
+        while(r<n){
+            if(hash[s[r]]>0) count++;
+            hash[s[r]]--;
+
+            while(count==m){
+                if(r-l+1<minlen){
+                    minlen = r-l+1;
+                    startIdx = l;
+                }
+
+                hash[s[l]]++;
+                if(hash[s[l]]>0) count--;
+                l++;
+            }
+            r++;
+        }
+        if(startIdx==-1) return "";
+        return s.substr(startIdx,minlen);
+    }
+};
